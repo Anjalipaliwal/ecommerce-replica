066eb98de2813766f49f590455a6ee02d1b9c3fd
Anjali Paliwal <Anjalipaliwal25@gmail.com>
2019-09-04 17:21:47 +0530

    infinite scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  private noOfItemsToShowInitially: number = 12;
+   private itemsToLoad: number = 12;  
   images = [];
   randomtext='Random Text';
+  
   constructor() {
     for (let i = 0; i < 50; i++) {
         const url = 'https://loremflickr.com/640/480?random=' + (i +1);
@@ -15,6 +19,18 @@ export class AppComponent {
           url: url,
           show: false
       };
+     
+     
     }
+    
   }
+  
+  
+  onScroll() {
+    if (this.noOfItemsToShowInitially <= this.images.length) {
+        this.noOfItemsToShowInitially += this.itemsToLoad;
+        console.log("scrolled");
+    } 
+
+}
 }
